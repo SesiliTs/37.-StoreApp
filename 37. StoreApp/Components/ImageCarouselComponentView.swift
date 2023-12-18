46d2babee60edd2aaa645a10e479086bfeec9f80
SesiliTs <sesili.tsikaridze.1@btu.edu.ge>
2023-12-19 03:42:57 +0400

    all pages with checkOut simulation

---
+++ 37. StoreApp/Components/ImageCarouselComponentView.swift
@@ -0,0 +1,18 @@
+//
+//  ImageCarouselComponentView.swift
+//  37. StoreApp
+//
+//  Created by Sesili Tsikaridze on 19.12.23.
+//
+
+import SwiftUI
+
+struct ImageCarouselComponentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageCarouselComponentView()
+}
---
+++ 37. StoreApp/Components/ImageFromURL.swift
@@ -0,0 +1,8 @@
+//
+//  ImageFromURL.swift
+//  37. StoreApp
+//
+//  Created by Sesili Tsikaridze on 19.12.23.
+//
+
+import Foundation
